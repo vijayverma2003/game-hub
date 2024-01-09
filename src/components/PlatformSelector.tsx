1b1473e2d9a1a5e21568b84ec526f6236d37e951
Vijay <vijayvermakvp@gmail.com>
2024-01-10 04:43:37 +0530

    Adding React Query

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -12,7 +12,7 @@ interface Props {
 const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
   const { data, error } = usePlatforms();
 
-  if (error) return;
+  if (error) return null;
 
   return (
     <Menu>
